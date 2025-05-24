fdeaaf2cd86908f1a3b83491d6742b72c403d2d3
SLY06600 <didimerlin@hotmail.fr>
2025-05-24 17:16:58 +0200

    first commit

---
+++ bonjour.f90
@@ -0,0 +1,16 @@
+program variables_demo
+    ! Déclaration des variables
+    integer :: age
+    real :: taille
+    character(len=20) :: nom
+
+    ! Affectation des valeurs
+    age = 25
+    taille = 1.75
+    nom = "Baraduc"
+
+    ! Affichage
+    print *, "Nom :", nom
+    print *, "Age :", age
+    print *, "Taille :", taille, "m"
+end program variables_demo
